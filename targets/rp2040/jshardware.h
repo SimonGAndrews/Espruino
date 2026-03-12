d606969592997d186d35486a405b473659e3b215
Your Name <you@example.com>
2026-03-12 00:55:27 +0000

    Add RP2040 delegated build and target skeleton

---
+++ targets/rp2040/jshardware.h
@@ -0,0 +1,7 @@
+#ifndef JSHARDWARE_RP2040_H
+#define JSHARDWARE_RP2040_H
+
+/* Keep this shim so local includes still work, but use Espruino's real API. */
+#include "../../src/jshardware.h"
+
+#endif
