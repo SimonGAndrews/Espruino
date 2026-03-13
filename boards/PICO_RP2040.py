718865d83c97c62654465bc4dba7da555cac0c26
Simon Andrews <Simon.g.andrews@live.co.uk>
2026-03-13 20:35:05 +0000

    rp2040: expand Pico board metadata and regen deps

--- boards/PICO_RP2040.py
+++ boards/PICO_RP2040.py
@@ -11,6 +11,8 @@ import pinutils
 
 info = {
   "name": "Raspberry Pi Pico (RP2040)",
+  "link": [ "https://espruino.com/Pico" ],
+  "espruino_page_link": "Pico",
   "boardname": "ESPR_PICO_RP2040",
   "default_console": "EV_USBSERIAL",
   "variables": 12000,
@@ -40,8 +42,35 @@ chip = {
 }
 
 devices = {
+  "LED1": { "pin": "D25" },
   "USB": {}
 }
+
+
+board = {
+  "left": [
+    "VBUS", "VSYS", "GND", "3V3_EN", "3V3", "ADC_VREF",
+    "D28", "AGND", "D27", "D26", "RUN",
+    "D22", "GND", "D21", "D20", "D19", "D18", "GND", "D17", "D16"
+  ],
+  "right": [
+    "D0", "D1", "GND", "D2", "D3", "D4", "D5", "GND", "D6", "D7",
+    "D8", "D9", "GND", "D10", "D11", "D12", "D13", "GND", "D14", "D15"
+  ],
+  "_notes": {
+    "D25": "Onboard LED on Raspberry Pi Pico",
+    "D26": "ADC0",
+    "D27": "ADC1",
+    "D28": "ADC2",
+    "RUN": "Pull low to reset the RP2040",
+    "D0": "Common debug UART TX during bring-up when early logging is enabled",
+    "D1": "Common debug UART RX during bring-up when early logging is enabled"
+  }
+}
+
+
+def add_pin_function(pins, pin_name, function_name, af=0):
+  pinutils.findpin(pins, pin_name, True)["functions"][function_name] = af
 
 
 def get_pins():
@@ -49,10 +78,87 @@ def get_pins():
   pins = pinutils.generate_pins(0, 29)
 
   # ADC channels on GPIO 26..29.
-  pinutils.findpin(pins, "PD26", True)["functions"]["ADC1_IN0"] = 0
-  pinutils.findpin(pins, "PD27", True)["functions"]["ADC1_IN1"] = 0
-  pinutils.findpin(pins, "PD28", True)["functions"]["ADC1_IN2"] = 0
-  pinutils.findpin(pins, "PD29", True)["functions"]["ADC1_IN3"] = 0
+  add_pin_function(pins, "PD26", "ADC1_IN0")
+  add_pin_function(pins, "PD27", "ADC1_IN1")
+  add_pin_function(pins, "PD28", "ADC1_IN2")
+  add_pin_function(pins, "PD29", "ADC1_IN3")
+
+  # The Pico's onboard LED is connected to GPIO25.
+  add_pin_function(pins, "PD25", "LED1")
+
+  # Common UART pin pairs exposed on Raspberry Pi Pico headers.
+  add_pin_function(pins, "PD0", "USART1_TX")
+  add_pin_function(pins, "PD1", "USART1_RX")
+  add_pin_function(pins, "PD4", "USART2_TX")
+  add_pin_function(pins, "PD5", "USART2_RX")
+  add_pin_function(pins, "PD8", "USART2_TX")
+  add_pin_function(pins, "PD9", "USART2_RX")
+  add_pin_function(pins, "PD12", "USART1_TX")
+  add_pin_function(pins, "PD13", "USART1_RX")
+  add_pin_function(pins, "PD16", "USART1_TX")
+  add_pin_function(pins, "PD17", "USART1_RX")
+  add_pin_function(pins, "PD20", "USART2_TX")
+  add_pin_function(pins, "PD21", "USART2_RX")
+  add_pin_function(pins, "PD24", "USART2_TX")
+  add_pin_function(pins, "PD25", "USART2_RX")
+  add_pin_function(pins, "PD28", "USART1_TX")
+  add_pin_function(pins, "PD29", "USART1_RX")
+
+  # Common I2C pin pairs exposed on Raspberry Pi Pico headers.
+  add_pin_function(pins, "PD0", "I2C1_SDA")
+  add_pin_function(pins, "PD1", "I2C1_SCL")
+  add_pin_function(pins, "PD2", "I2C2_SDA")
+  add_pin_function(pins, "PD3", "I2C2_SCL")
+  add_pin_function(pins, "PD4", "I2C1_SDA")
+  add_pin_function(pins, "PD5", "I2C1_SCL")
+  add_pin_function(pins, "PD6", "I2C2_SDA")
+  add_pin_function(pins, "PD7", "I2C2_SCL")
+  add_pin_function(pins, "PD8", "I2C1_SDA")
+  add_pin_function(pins, "PD9", "I2C1_SCL")
+  add_pin_function(pins, "PD10", "I2C2_SDA")
+  add_pin_function(pins, "PD11", "I2C2_SCL")
+  add_pin_function(pins, "PD12", "I2C1_SDA")
+  add_pin_function(pins, "PD13", "I2C1_SCL")
+  add_pin_function(pins, "PD14", "I2C2_SDA")
+  add_pin_function(pins, "PD15", "I2C2_SCL")
+  add_pin_function(pins, "PD16", "I2C1_SDA")
+  add_pin_function(pins, "PD17", "I2C1_SCL")
+  add_pin_function(pins, "PD18", "I2C2_SDA")
+  add_pin_function(pins, "PD19", "I2C2_SCL")
+  add_pin_function(pins, "PD20", "I2C1_SDA")
+  add_pin_function(pins, "PD21", "I2C1_SCL")
+  add_pin_function(pins, "PD22", "I2C2_SDA")
+  add_pin_function(pins, "PD23", "I2C2_SCL")
+  add_pin_function(pins, "PD24", "I2C1_SDA")
+  add_pin_function(pins, "PD25", "I2C1_SCL")
+  add_pin_function(pins, "PD26", "I2C2_SDA")
+  add_pin_function(pins, "PD27", "I2C2_SCL")
+  add_pin_function(pins, "PD28", "I2C1_SDA")
+  add_pin_function(pins, "PD29", "I2C1_SCL")
+
+  # Common SPI pin groups exposed on Raspberry Pi Pico headers.
+  add_pin_function(pins, "PD0", "SPI1_MISO")
+  add_pin_function(pins, "PD2", "SPI1_SCK")
+  add_pin_function(pins, "PD3", "SPI1_MOSI")
+  add_pin_function(pins, "PD4", "SPI1_MISO")
+  add_pin_function(pins, "PD6", "SPI1_SCK")
+  add_pin_function(pins, "PD7", "SPI1_MOSI")
+  add_pin_function(pins, "PD8", "SPI2_MISO")
+  add_pin_function(pins, "PD10", "SPI2_SCK")
+  add_pin_function(pins, "PD11", "SPI2_MOSI")
+  add_pin_function(pins, "PD12", "SPI2_MISO")
+  add_pin_function(pins, "PD14", "SPI2_SCK")
+  add_pin_function(pins, "PD15", "SPI2_MOSI")
+  add_pin_function(pins, "PD16", "SPI1_MISO")
+  add_pin_function(pins, "PD18", "SPI1_SCK")
+  add_pin_function(pins, "PD19", "SPI1_MOSI")
+  add_pin_function(pins, "PD20", "SPI1_MISO")
+  add_pin_function(pins, "PD22", "SPI1_SCK")
+  add_pin_function(pins, "PD23", "SPI1_MOSI")
+  add_pin_function(pins, "PD24", "SPI2_MISO")
+  add_pin_function(pins, "PD26", "SPI2_SCK")
+  add_pin_function(pins, "PD27", "SPI2_MOSI")
+  add_pin_function(pins, "PD28", "SPI2_MISO")
 
   for pin in pins:
     pin["functions"]["3.3"] = 0
