cef295afeabba74e2b4b329bd51b8ba78ca1a182
Simon Andrews <55559464+SimonGAndrews@users.noreply.github.com>
2026-05-14 12:25:54 +0100

    ESP32C3_IDF5: keep USB/JTAG console init buildable on IDF4

--- targets/esp32/jshardwareUart.c
+++ targets/esp32/jshardwareUart.c
@@ -104,14 +104,19 @@ void initSerial(IOEventFlags device, JshUSARTInfo *inf){
 void initConsole(){
 #ifdef ESPR_USE_USB_SERIAL_JTAG
   /* Configure USB-CDC driver */
+  usb_serial_jtag_driver_config_t usb_serial_config = {
+    .tx_buffer_size = 128,
+    .rx_buffer_size = 128
+  };
+#if ESP_IDF_VERSION_MAJOR>=5
   if (!usb_serial_jtag_is_driver_installed()) {
-    usb_serial_jtag_driver_config_t usb_serial_config = {
-      .tx_buffer_size = 128,
-      .rx_buffer_size = 128
-    };
     jsDebug(DBG_INFO, "initConsole: Installing usb_serial_jtag_driver\n");
     ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_config));
   }
+#else
+  jsDebug(DBG_INFO, "initConsole: Installing usb_serial_jtag_driver\n");
+  ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_config));
+#endif
 #endif
   uart_config_t uart_config = {
     .baud_rate = 115200,
